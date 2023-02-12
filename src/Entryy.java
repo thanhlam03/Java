a6536d0a77f862c24b48964b59f189382e1efbf9
thanhlam03 <122240411+thanhlam03@users.noreply.github.com>
2023-02-12 22:50:11 +0700

    add

---
+++ src/Entryy.java
@@ -0,0 +1,9 @@
+public class Entryy {
+    public static void main(String[] args) {
+        Student s = new Student("ThanhLam", 20, "Quang Binh", 6.5);
+        s.display();
+        System.out.println();
+        Teacher t = new Teacher("Tuyen", 34, "Quang Binh", 1900);
+        t.display();
+    }
+}
